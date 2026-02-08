1f6195dd682645c7785f51300fc886149baca6f5
sanjeev kumar <uiet2k7sanjeev@gmail.com>
2026-02-08 22:04:03 +0530

    Update Palmerpenguins Visualization in R.R

--- Palmerpenguins Visualization in R.R
+++ Palmerpenguins Visualization in R.R
@@ -4,4 +4,15 @@ ggplot(data = penguins)+ geom_point(mapping=aes(x=flipper_len,y=body_mass))
 ggplot(data = penguins) + 
   geom_point(mapping = aes(x=flipper_len, y=body_mass,shape = species,color=species))
 ggplot(data =penguins)+
-  geom_point(mapping= aes(x=flipper_len,y=body_mass,alpha = species))
+  geom_point(mapping= aes(x=flipper_len,y=body_mass,alpha = species))library(palmerpenguins)
+library(ggplot2)
+library(palmerpenguins)
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+
+  geom_point(aes(color = species))+
+  facet_wrap(~species)
+ggplot(data=diamonds)+
+  geom_bar(mapping = aes(x=color,fill = cut))+
+  facet_wrap(~cut)
+ggplot(data=penguins,aes(x=flipper_length_mm,y=body_mass_g))+
+  geom_point(aes(color = species))+
+  facet_grid(sex~species)
